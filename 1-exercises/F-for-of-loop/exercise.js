675efaa20e27e289a812ef859e0e8426815b7045
Barath Vignarajah <moneyinthesky@gmail.com>
2021-12-29 15:22:14 +0000

    Adding extra exercises

---
+++ 1-exercises/F-for-of-loop/exercise.js
@@ -0,0 +1,13 @@
+/*
+    A for-of loop is a easy and way of looping through the elements of an array, string or any other "iterable object" (think sequence of elements).    
+*/
+
+let tubeStations = [
+    "Aldgate",
+    "Baker Street",
+    "Picadilly Circus",
+    "Oxford Street",
+    "Tottenham Court Road"
+];
+
+// TODO Use a for-of loop to output each of the tube stations above.
